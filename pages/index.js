33eac57928042fedcfeb93388dc0888bd78ef5aa
djurasovicvedran <djurasovic.vedran@gmail.com>
2022-07-28 23:07:47 +0200

    init

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,18 @@ import { client } from "../lib/client";
 const Home = ({ products, bannerData }) => {
   return (
     <Fragment>
-      <HeroBanner />
-      {console.log(bannerData)}
+      <HeroBanner heroBanner={bannerData.length > 0 && bannerData[0]} />
+
       <div className="products-heading">
         <h2>Best Sellers</h2>
         <p>You won't ever regret buying this</p>
       </div>
-      <div className="products-container"></div>
-      <FooterBanner />
+      <div className="products-container">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </Fragment>
   );
 };
